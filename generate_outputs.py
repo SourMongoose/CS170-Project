93467b29d7242b2bfec25262f57c6a67fcd927e6
Rahul Kumar <rahulkumar@berkeley.edu>
2019-12-05 11:26:36 -0800

    TSPY library solver

--- generate_outputs.py
+++ generate_outputs.py
@@ -1,6 +1,7 @@
 import solver
 import os
 import multiprocessing as mp
+import sys, traceback
 
 alg = solver.NaiveSolverMultiple
 
@@ -10,7 +11,15 @@ try:
 except:
     pass
 
+parallelize = False
+
 bad = set([19,47,59,69,80,91,92,119,129,138,143,158,159,176,193,198,215,221,225,243,257,270,280,283,292,302,308,310,312,313,317,323,328,330,333,336,338,339,341,344,346,347,348,349,353,354,355])
+
+def handle_err(e, i):
+    print("Exception occurred on input {0}:".format(i))
+    print("-"*60)
+    traceback.print_exc(file=sys.stdout)
+    print("-"*60)
 
 def solve(i):
     if i%10==0:
@@ -21,22 +30,25 @@ def solve(i):
     try:
         g1 = alg('inputs/{0}_50.in'.format(i))
         g1.solveAndOutput('outputs/{0}_50.out'.format(i))
-    except:
-        pass
+    except Exception as e:
+        handle_err(e, i)
 
     try:
         g1 = alg('inputs/{0}_100.in'.format(i))
         g1.solveAndOutput('outputs/{0}_100.out'.format(i))
-    except:
-        pass
+    except Exception as e:
+        handle_err(e, i)
 
     try:
         g1 = alg('inputs/{0}_200.in'.format(i))
         g1.solveAndOutput('outputs/{0}_200.out'.format(i))
-    except:
-        pass
+    except Exception as e:
+        handle_err(e, i)
 
-num_cpu = mp.cpu_count()
-
-with mp.Pool(processes=num_cpu) as p:
-    p.map(solve, list(range(1, 367)))
+if parallelize:
+    num_cpu = mp.cpu_count()
+    with mp.Pool(processes=num_cpu) as p:
+        p.map(solve, list(range(1, 367)))
+else:
+    for i in range(1, 367):
+        solve(i)
